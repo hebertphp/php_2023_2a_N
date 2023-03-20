5fb5ff764b53ef92374de765f87ee66fa8be3acd
Prof. Hebert <hebertphp@gmail.com>
2023-03-20 20:10:14 -0300

    20-03-23

--- aula_20-03-23/pg9.php
+++ aula_20-03-23/pg9.php
@@ -1,7 +1,7 @@
 <?php
-$n1=10;
-$n2=010;
-$n3=0x10;
-$n4=0b10;
+$n1=10;#base decimal 0123456789 10
+$n2=010;#base octal 01234567 10(8)
+$n3=0x10;#base hexadecimal 0123456789ABCDEF 10(16)
+$n4=0b10;#base binários 01 10(2)
 echo "n1=$n1<br>n2=$n2<br>n3=$n3<br>n4=$n4";
 ?>
