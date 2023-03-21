4f24d4bd80ad6153e5116fc082a8d9cd282d7e37
Prof. Hebert <hebertphp@gmail.com>
2023-03-20 21:59:33 -0300

    20-03-23

--- aula_20-03-23/pg04_01.php
+++ aula_20-03-23/pg04_01.php
@@ -1,10 +1,18 @@
 <?php
-$a=10;
-$b=2;
+$a=5;
+$b=3;
 echo "Adição:".($a + $b);
 echo "<br>Subtração:".($a - $b);
 echo "<br>Multiplicação:".($a * $b);
 echo "<br>Divisão:".($a / $b);
 echo "<br>Módulo:".($a % $b);
 echo "<br>Exponenciação:".($a ** $b);
+ /*
+
+ 123563736   9%2 = 1 (ímpar)
+ 7987896     8%2 = 0 (par)
+
+ 5%3=2
+
+ */
 ?>
---
+++ aula_20-03-23/pg04_01e.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Calcular a média de duas notas
+Saída, mostrar as duas notas e a média calculada
+*/
+
+?>
