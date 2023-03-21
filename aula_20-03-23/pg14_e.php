280201ac4413f673a7821d1459555c2f8d1f41c3
Prof. Hebert <hebertphp@gmail.com>
2023-03-20 21:15:35 -0300

    20-03-23

---
+++ aula_20-03-23/pg14_e.php
@@ -0,0 +1,15 @@
+<?php
+/*
+criar três constantes para armazenar:
+nome, unidade e período
+
+Imprimir:
+Meu nome é Hebert, estudo na unidade Memorial
+no período noturno.
+
+Valores nas constantes:
+nome - Hebert
+unidade - Memorial
+período - Noturno
+*/
+?>
