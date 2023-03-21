b93c36b0f8f87e2daa6836283aee462ab57886f6
Prof. Hebert <hebertphp@gmail.com>
2023-03-20 21:12:12 -0300

    20-03-23

--- aula_20-03-23/pg14.php
+++ aula_20-03-23/pg14.php
@@ -1,6 +1,8 @@
 <?php
 define("PI", 3.14);
 define("HOST","localhost");
+define("USUARIO","Bete");
 $valor = 2 * PI;
-echo HOST . " - ". $valor;
+echo HOST . " - ". $valor."<br>";
+echo "Usuario: ".USUARIO;
 ?>
