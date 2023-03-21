8b3d3c7d3959a0f369fc7c612f4adff544d8ba09
Prof. Hebert <hebertphp@gmail.com>
2023-03-20 21:46:07 -0300

    20-03-23

--- aula_20-03-23/pg14_e.php
+++ aula_20-03-23/pg14_e.php
@@ -1,4 +1,9 @@
 <?php
+define("NOME","Hebert");
+define("UNIDADE","Memorial");
+define("PERIODO","noturno");
+echo "Meu nome é ".NOME.", estudo na unidade ".UNIDADE."
+no período ".PERIODO;
 /*
 criar três constantes para armazenar:
 nome, unidade e período
