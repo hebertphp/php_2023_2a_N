77230e5fb8b6ec850697e4a8b46220c080fdcc62
Prof. Hebert <hebertphp@gmail.com>
2023-03-27 19:57:36 -0300

    27/03/23

---
+++ aula_27-03-23/pg04_01e.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Calcular a média de duas notas
+Saída, mostrar as duas notas e a média calculada
+*/
+$nota1=7;
+$nota2=8;
+$media=$nota1+$nota2/2;
+echo "Nota1=$nota1, Nota2=$nota2<br>";
+echo "Media=$media";
+?>
