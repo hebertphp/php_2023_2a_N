f57ba046dce870d5c193893c03411e52698a1767
Prof. Hebert <hebertphp@gmail.com>
2023-03-27 21:33:25 -0300

    27/03/23

--- aula_27-03-23/pg04_032.php
+++ aula_27-03-23/pg04_032.php
@@ -1,11 +1,8 @@
 <?php
 $endereco="Rua das Camélias ";
-echo $endereco;
-$endereco=" número 122";
-echo $endereco;
-$endereco=" Bairro Pinheiros ";
-echo $endereco;
-$endereco=" perto da Casa Verde ";
+$endereco.=" número 122";
+$endereco.=" Bairro Pinheiros ";
+$endereco.=" perto da Casa Verde ";
 echo $endereco;
 /*
 Como fazer para imprimir o endereço completo
