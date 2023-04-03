aab1052cafe6b7ac42f07b8921e0ff573b36d544
Prof. Hebert <hebertphp@gmail.com>
2023-04-03 20:19:33 -0300

    03/04/23

--- aula_03_04_23/pg04_05.php
+++ aula_03_04_23/pg04_05.php
@@ -1,12 +1,14 @@
 <?php
 $a = 10;
 $b = "10";
-$c = $a===$b;
+$c = $a!==$b;
 var_dump($c);
 /*
 =   atribuição(recebe)
 ==  igualdade verifica apenas o valor
 === idêntico varifica o valor e o tipo
 
+!=  não igual (diferentes)
+!== não idêntico
 */
 ?>
