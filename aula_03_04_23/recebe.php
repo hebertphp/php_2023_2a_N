4512b87d7a4e08eb35327595edb2f3bac33168ad
Prof. Hebert <hebertphp@gmail.com>
2023-04-03 21:26:23 -0300

    03/04/23

--- aula_03_04_23/recebe.php
+++ aula_03_04_23/recebe.php
@@ -1,3 +1,10 @@
 <?php
 echo "Recebe<br>";
+$ano_atual=$_GET["ano_atual"];
+$ano_nascimento=$_GET["ano_nascimento"];
+echo "$ano_atual ,$ano_nascimento ";
+$idade=$ano_atual-$ano_nascimento;
+$cond=$idade>=18;
+echo "Eu tenho $idade anos, será que posso assitir o filme?";
+var_dump($cond);
 ?>
