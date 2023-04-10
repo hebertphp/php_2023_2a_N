d157bd5080a9c7810d4c7047a5fb3f30ee9b8b22
Prof. Hebert <hebertphp@gmail.com>
2023-04-10 20:49:42 -0300

    10/04/23

--- aula_10-04-23/recebe.php
+++ aula_10-04-23/recebe.php
@@ -1,4 +1,15 @@
 <?php
-echo "recebe";
+//echo count($_POST);
+if(count($_POST)==0){
+    echo "<h1>Acesse o form primeiro<h1>";
+    echo "<a href='form.html'>Formulário</a>";
+    exit();
+}
+
+$matric=$_POST["matric"];
+$nome=$_POST["nome"];
+$telefone=$_POST["telefone"];
+$endereco=$_POST["endereco"];
+$uf=$_POST["uf"];
 
 ?>
