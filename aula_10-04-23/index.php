176f536a342044c3117e62eece57246b3d36f21b
Prof. Hebert <hebertphp@gmail.com>
2023-04-10 19:58:34 -0300

    10/04/23

---
+++ aula_10-04-23/index.php
@@ -0,0 +1,15 @@
+<?php
+/*
+https://github.com/hebertphp/php_2023_2a_N
+Criar um formulário form.html contendo 3 campos:
+    RA
+    NOME
+    CPF
+Criar um arquivo recebe.php para receber e
+imprimir os três valores digitados nos campos,
+para montar um insert!!!   
+Usar os códigos da aula 03_04_23 como base!
+*/
+?>
+<hr>
+<a href="form.html">form</a><br>
