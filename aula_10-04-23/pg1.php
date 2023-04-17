09c1201223e59502f274224588542d4c471790be
Prof. Hebert <hebertphp@gmail.com>
2023-04-17 19:52:54 -0300

    17/04/23

--- aula_10-04-23/pg1.php
+++ aula_10-04-23/pg1.php
@@ -2,7 +2,7 @@
 $a=50;
 $b=120;
 $c=200;
-$d=($a<=$b) ? "Verdadeiro" : "Falso";
-$e=($a>=$c) ? "Verdadeiro" : "Falso";
+$d=!($a<=$b) ? "OK!" : "Só que não!";
+$e=!($a>=$c) ? "Blz!" : "Zoado!";
 echo "d = ".$d."<br>e = ".$e;
 ?>
---
+++ aula_17-04-23/index.php
@@ -0,0 +1,4 @@
+<?php
+echo "PHP - 17/04/23";
+?><hr>
+<a href="pg1.php">pg1</a><br>
