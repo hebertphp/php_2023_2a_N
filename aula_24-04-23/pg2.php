47a270f62e41172ffd114dbaea5c8d6d5e36e6e8
Prof. Hebert <hebertphp@gmail.com>
2023-04-24 20:30:07 -0300

    24/04/23

--- aula_24-04-23/pg2.php
+++ aula_24-04-23/pg2.php
@@ -1,7 +1,14 @@
 <?php
-$i=0; // inicialização do contador;
+$i=0; 
 while ($i <= 5) {
     echo   "$i ";
     $i++;
 }
+/* 
+1) Qual o valor inicial?
+2) Qual a condição?
+3) Qual é o contador?
+4) Quantas vezes o looping foi executado?
+5) Qual o valor que tornou a condição como falsa?
+*/
 ?>
