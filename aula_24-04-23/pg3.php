2ea13dadc80ec9831936ac9d089e9a4c7b6ded7a
Prof. Hebert <hebertphp@gmail.com>
2023-04-24 21:24:00 -0300

    24/04/23

--- aula_24-04-23/pg3.php
+++ aula_24-04-23/pg3.php
@@ -1,14 +1,16 @@
 <?php
-$i=0; 
+$i=7; 
 do {
     echo "$i ";
     $i++; 
 } while ($i < 5);
+echo "<hr>";
 /*
-1) Qual o valor inicial? 
-2) Qual a condição? 
-3) Qual é o contador? 
-4) Quantas vezes o looping foi executado? 
-5) Qual o valor que tornou a condição como falsa? 
+Saída: 0 1 2 3 4
+1) Qual o valor inicial? 0
+2) Qual a condição? $i<5
+3) Qual é o contador? $i++
+4) Quantas vezes o looping foi executado? 5
+5) Qual o valor que tornou a condição como falsa? 5
 */
 ?>
