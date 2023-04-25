65f822d32a7dee74d6b124a5d17edaa560986606
Prof. Hebert <hebertphp@gmail.com>
2023-04-24 21:44:05 -0300

    24/04/23

--- aula_24-04-23/pg4.php
+++ aula_24-04-23/pg4.php
@@ -1,5 +1,23 @@
 <?php
-for ($i=0; $i<6; $i++) {
+for ($i=0; $i<6; $i++) { 
     echo "$i  ";
+}
+
+/*
+Gerar um código para mostrar a saida abaixo:
+$tabuada=2;
+
+2 x 0 = 0
+2 x 1 = 2
+2 x 2 = 4
+2 x 3 = 6
+......
+2 x 10 = 20
+
+*/
+$tabuada=2;
+echo "<h1>Tabuada do $tabuada</h1>";
+for($i=0;$i<=10;$i++){
+    echo "$tabuada x $i = ".$tabuada * $i."<br>";
 }
 ?>
