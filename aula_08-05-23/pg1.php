6e0510eaf3339ddef2003d3ecad45b538e0c8f72
Prof. Hebert <hebertphp@gmail.com>
2023-05-08 20:37:31 -0300

    08/05/23

---
+++ aula_08-05-23/pg1.php
@@ -0,0 +1,12 @@
+<?php
+$x[0]=8;
+$x[1]=2023;
+$x[2]="Maio";
+echo $x[1];
+echo "<hr>";
+//         0    1    2    3    
+$uf=array("SP","RJ","MG","ES");
+echo $uf[2];
+echo "<hr>";
+print_r($uf);
+?>
