4f8a92113fe2bfc27cbd274e85f184bc7c7c6dbd
Prof. Hebert <hebertphp@gmail.com>
2023-05-08 20:59:33 -0300

    08/05/23

--- aula_08-05-23/pg1.php
+++ aula_08-05-23/pg1.php
@@ -8,5 +8,13 @@ echo "<hr>";
 $uf=array("SP","RJ","MG","ES");
 echo $uf[2];
 echo "<hr>";
-print_r($uf);
+echo "<pre>";print_r($uf);echo "</pre>";
+echo "<hr>";
+$num=array(10,20,7,300);
+echo "<pre>";print_r($num);echo "</pre>";
+$num[0]+=5;//[0]=15
+$num[1]+=$num[0];//[1]=35
+$num[2]%=2;//[2]=1
+$num[3]/=10;//[3]=30
+echo "<pre>";print_r($num);echo "</pre>";
 ?>
