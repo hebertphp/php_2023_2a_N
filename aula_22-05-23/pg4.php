7037ccf0ff1e11930107cfb70ac63db5a554e583
Prof. Hebert <hebertphp@gmail.com>
2023-05-22 20:48:46 -0300

    22/05/23

--- aula_22-05-23/pg4.php
+++ aula_22-05-23/pg4.php
@@ -1,6 +1,11 @@
 <?php
-$uf = array("SP","RJ","MG","ES");
+$uf = array("SP","RJ","MG","ES","MT","RS","DF");
+//       array   simples    
 foreach ($uf as $valor){
     echo $valor . "<br>";
+}
+echo "<hr>";
+foreach($uf as $ind=>$valor){
+    echo "[$ind]=+.or<br>";
 }
 ?>
