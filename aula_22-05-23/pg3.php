e2401d1e2d00cfb54b4811f4449cf600416b6b51
Prof. Hebert <hebertphp@gmail.com>
2023-05-22 20:34:36 -0300

    22/05/23

---
+++ aula_22-05-23/pg3.php
@@ -0,0 +1,8 @@
+<?php
+$game=array(
+            array("água","água","submarino"),
+            array("porta-aviões","água","água"),
+            array("água","destroyer","água")    
+            );
+echo $game[2][1];//destroyer
+?>
