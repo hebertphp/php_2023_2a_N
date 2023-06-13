2b6707272c4e9b40fddcde7418e2be06b1620cf6
Prof. Hebert <hebertphp@gmail.com>
2023-06-12 21:31:33 -0300

    12/06/23

--- aula_12-06-23/recebe_insere.php
+++ aula_12-06-23/recebe_insere.php
@@ -1,4 +1,17 @@
 <?php
-echo "<pre>";print_r($_POST);echo "</pre>"; 
+//echo "<pre>";print_r($_POST);echo "</pre>"; 
+include "conecta.php";
 
+$login=$_POST["login"];
+$senha=$_POST["senha"];
+$email=$_POST["email"];
+
+$sql="
+insert into usuario (id,login,senha,email) 
+            values (null,$login,$senha,$email)
+";
+echo "<hr>$sql<hr>";
+$query = mysqli_query($conn,$sql) or die (mysqli_error($conn));
+echo "Insert OK!";
+mysqli_close($conn);
 ?>
