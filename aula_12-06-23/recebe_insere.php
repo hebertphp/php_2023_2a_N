749a730cd4e6639a0d6711dc7528b740578d528a
Prof. Hebert <hebertphp@gmail.com>
2023-06-19 20:07:52 -0300

    19/06/23

--- aula_12-06-23/recebe_insere.php
+++ aula_12-06-23/recebe_insere.php
@@ -8,9 +8,9 @@ $email=$_POST["email"];
 
 $sql="
 insert into usuario (id,login,senha,email) 
-            values (null,$login,$senha,$email)
+            values (null,'$login','$senha','$email')
 ";
-echo "<hr>$sql<hr>";
+//echo "<hr>$sql<hr>";
 $query = mysqli_query($conn,$sql) or die (mysqli_error($conn));
 echo "Insert OK!";
 mysqli_close($conn);
